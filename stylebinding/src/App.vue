a6fed068aa40432b19166d9aae04f3e222c39f86
minaxi.machhi@bacancytechnology.com <[minaxi.machhi@bacancytechnology.com]>
2020-02-13 17:17:25 +0530

    styleBindingTask2

---
+++ stylebinding/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <styleBinding/>
+  </div>
+</template>
+<script>
+import styleBinding from '@/components/styleBinding.vue'
+export default {
+  components: {
+    styleBinding
+  }
+}
+</script>
---
+++ stylebinding/src/components/styleBinding.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div>
+      <select @change="changeColor($event)">
+        <option value="color">Color</option>
+        <option :value="colors.color" v-for="(inx,color) in colors" :key="color">{{colors[color]}}</option>
+      </select>
+
+      <select @change="changeSize($event)">
+        <option value="size">Size</option>
+        <option :value="sizes.size" v-for="(inx,size) in sizes" :key="size">{{sizes[size]}}</option>
+      </select>
+      <select @change="changeBg($event)">
+        <option value="background">background</option>
+        <option
+          :value="backgrounds.background"
+          v-for="(inx,background) in backgrounds"
+          :key="background"
+        >{{backgrounds[background]}}</option>
+      </select>
+      is Bold
+      <input type="radio" name="Bold" value="bold" @change="isBold($event)" />Yes
+      <input type="radio" name="Bold" value="" @change="isBold($event)" />No
+    </div>
+    <br />
+    <br />
+    <center>
+      <div ref="content">
+        abcd efgh ijkl mnop qrst uvwx yz
+        <br />abcd efgh ijkl mnop qrst uvwx yz
+        <br />abcd efgh ijkl mnop qrst uvwx yz
+        <br />abcd efgh ijkl mnop qrst uvwx yz
+        <br />abcd efgh ijkl mnop qrst uvwx yz
+      </div>
+    </center>
+  </div>
+</template>
+<script>
+/*eslint-disable*/
+export default {
+  data() {
+    return {
+      colors: { color1: "red", color2: "orange", color3: "green" },
+      sizes: { size1: "12px", size2: "24px", size3: "26px", size4: "38px" },
+      backgrounds: { bg1: "yellow", bg2: "pink", bg3: "violet" }
+    };
+  },
+  methods: {
+    changeColor(event) {
+      this.$refs.content.style.color =
+        event.target.options[event.target.options.selectedIndex].text;
+    },
+    changeSize(event) {
+      this.$refs.content.style.fontSize =
+        event.target.options[event.target.options.selectedIndex].text;
+    },
+    changeBg(event) {
+      this.$refs.content.style.background =
+        event.target.options[event.target.options.selectedIndex].text;
+    },
+    isBold(event) {
+        console.log(event.target.value)
+      this.$refs.content.style.fontWeight = event.target.value;
+    }
+  }
+};
+</script>
